347c7dca0067d1a614db70f22bd01681ce99ac5d
woogeunpeter <choiwoogeunpeter@gmail.com>
2025-08-10 12:58:27 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,16 +1,15 @@
-const CACHE = 'patent-ox-pro-v3';
+const CACHE = 'patent-ox-pro-v4';
+const toAbs = p => new URL(p, self.location).toString();
 const ASSETS = [
-  '/', '/index.html', '/manifest.webmanifest', '/sw.js',
-  '/icon-192.png', '/icon-512.png',
-  '/data/questions.json', '/data/cases.json'
-];
+  'index.html','manifest.webmanifest','sw.js',
+  'icon-192.png','icon-512.png',
+  'data/questions.json','data/cases.json'
+].map(toAbs);
 
 self.addEventListener('install', e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
 });
-self.addEventListener('activate', e=>{
-  e.waitUntil(self.clients.claim());
-});
+self.addEventListener('activate', e=>{ e.waitUntil(self.clients.claim()); });
 self.addEventListener('fetch', e=>{
   e.respondWith(
     caches.match(e.request).then(res=>{
@@ -18,7 +17,7 @@ self.addEventListener('fetch', e=>{
         const copy = net.clone();
         caches.open(CACHE).then(c=>c.put(e.request, copy));
         return net;
-      }).catch(()=>caches.match('/index.html'));
+      }).catch(()=>caches.match(toAbs('index.html')));
     })
   );
 });
